d425da5a2fa21b2e4c20bcae4e9280e929244854
YuTengjing <ytj2713151713@gmail.com>
2024-01-23 13:12:26 +0800

    refactor: simplify benchmark code

---
+++ .changeset/clever-seas-clap.md
@@ -0,0 +1,5 @@
+---
+'fetch-github-trending': patch
+---
+
+simplify benchmark code
--- benchmark/index.ts
+++ benchmark/index.ts
@@ -9,7 +9,7 @@ let start = performance.now();
 await $`node ${outFile}`;
 const current = performance.now();
 const nodeCosts = current - start;
-console.log(`nodejs costs ${nodeCosts}ms`);
+console.log(`node costs ${nodeCosts}ms`);
 
 // bun
 start = current;
@@ -17,25 +17,10 @@ await $`bun ${outFile}`;
 const bunCosts = performance.now() - start;
 console.log(`bun costs ${bunCosts}ms`);
 
-const result =
-    nodeCosts > bunCosts
-        ? {
-              faster: 'bun',
-              slower: 'nodejs',
-              fastCosts: bunCosts,
-              slowerCosts: nodeCosts,
-              rate: (nodeCosts - bunCosts) / bunCosts,
-          }
-        : {
-              faster: 'nodejs',
-              slower: 'bun',
-              fasterCosts: nodeCosts,
-              slowerCosts: bunCosts,
-              rate: (bunCosts - nodeCosts) / nodeCosts,
-          };
+const [fasterCosts, slowerCosts] =
+    nodeCosts > bunCosts ? [bunCosts, nodeCosts] : [nodeCosts, bunCosts];
+const [faster, slower] = fasterCosts === nodeCosts ? ['node', 'bun'] : ['bun', 'node'];
+const rate = `${(((slowerCosts - fasterCosts) / fasterCosts) * 100).toFixed(3)}%`;
 
-console.log(
-    `${result.faster}(${result.fastCosts}) is faster than ${result.slower}(${
-        result.slowerCosts
-    }) with ${(result.rate * 100).toFixed(3)}%`,
-);
+// biome-ignore format: readability
+console.log(`${faster}(${fasterCosts.toFixed(3)}) is faster than ${slower}(${slowerCosts.toFixed(3)}) with ${rate}`);
